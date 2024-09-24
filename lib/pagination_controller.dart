0b4239ae0816bebf20793e133cda2a70244d2a56
Andrey Vakarchuk <dronn1k.dev@gmail.com>
2024-09-24 16:23:01 +0300

    main export file has been fixed.

--- lib/pagination_controller.dart
+++ lib/pagination_controller.dart
@@ -1,6 +1,7 @@
 library pagination_controller;
 
-export 'src/base/pagination_controller_result.dart';
-export 'src/base/pagination_controller_state.dart';
-export 'src/base/pagination_method.dart';
+export 'src/base/pagination_controller_base.dart';
 export 'src/controller/cubit_pagination_controller.dart';
+export 'src/controller/flutter_pagination_controller.dart';
+export 'src/widget/cubit_paginated_list_builder.dart';
+export 'src/widget/flutter_paginated_list_builder.dart';
